6380b8d6f3b18cb1fae447fa59148c26322d2814
Annhe <tecbbs@qq.com>
2020-05-17 22:49:23 +0800

    fix: <br/> replace

--- plot.php
+++ plot.php
@@ -75,7 +75,7 @@ class WordpressPlot {
 
 function plotRender($atts, $content=null) {
 	$plot = new WordpressPlot($atts);
-	$content=preg_replace('/<br \/>/','',$content);
+	$content=preg_replace('/<br\s?\/>/','',$content);
 	$content=preg_replace('/&#822(0|1);/','"',$content);
 	$content=preg_replace('/&#8243;/','"',$content);
 	$content=preg_replace('/&#8212;/','--',$content);
